4df3be19b392c7f9346717d2f97dd46db26399b7
danielrh <danielryanh7@gmail.com>
2022-07-22 00:18:07 -0400

    working truss solver, just need to model the truss and we can use this to solve the system

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-import graphs
+import TrussSolver as ts
+import numpy as np
 
-# Tension will be +ve and Compression -ve
-MAX_TENSION = 12000
-MAX_COMPRESSION = -8000
+nodes = np.array([
+    [0,0],      #0
+    [4,0],      #1
+    [0,4],      #2
+    [4,4],      #3
+    [0,8],      #4
+    [4,8],      #5
+    [4,12],     #6
+    [8,12],     #7
+    [8,8],      #8
+    [12,8]      #9
+], dtype=object)
 
+connections = np.array([
+    [1,2,3],        #0
+    [0,3],          #1
+    [0,3,4],        #2
+    [0,1,2,4,5],    #3
+    [2,3,5,6],      #4
+    [3,4,6,7,8],    #5
+    [4,5,7],        #6
+    [6,5,8,9],      #7
+    [5,7,9],        #8
+    [7,8]           #9
+],dtype=object)
 
-class Member:
-    def __init__(self, joints, magnitude):
-        self.joints = joints
-        self.magnitude = magnitude
-        self.force = 0
-        pass
+truss1 = ts.Truss(nodes, connections)
 
+truss1.addLoad(0,[-60,-280])
+truss1.addLoad(1,[0,360])
+truss1.addLoad(9,[60,-80])
+truss1.solve()
 
-class Joint:
-    def __init__(self, x, y, numMembers, firstMember, secondMember, thirdMember):
-        self.x = x
-        self.y = y
-        self.numMembers = numMembers
-        self.firstMember = firstMember
-        self.secondMember = secondMember
-        self.thirdMember = thirdMember
-        pass
+trussdraw = ts.PgTruss(truss1,800)
+trussdraw.drawNodes()
 
-
-class Bridge:
-    def __init__(self):
-        pass
+"""
+Blue is compression
+Red is tension
+"""
